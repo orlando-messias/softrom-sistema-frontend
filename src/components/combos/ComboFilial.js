4906413e2ae409db0664f463980e2a8bdbc65aad
orlando-messias <programadorlando@gmail.com>
2021-05-11 09:34:09 -0300

    modal contrato e combo filial

--- src/components/combos/ComboFilial.js
+++ src/components/combos/ComboFilial.js
@@ -2,14 +2,20 @@ import React, { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
+import Button from "@material-ui/core/Button";
 // services
 import api from "../../services/api";
+import { Formik, Form } from "formik";
 
 const ComboFilial = (props) => {
   const [data, setData] = useState([]);
 
   const [inputValue, setInputValue] = useState("");
   const [valueSelect, setValueSelect] = useState(props.filial);
+
+  const initialValues = {
+    filial: { id: '', descricao: '' }
+  };
 
   const user = useSelector((state) => state.loginReducer.user);
   const origin_id = useSelector((state) => state.loginReducer.origin);
@@ -40,29 +46,40 @@ const ComboFilial = (props) => {
     setValueSelect(props.filial);
   }, [props.filial]);
 
+  const submit = values => {
+    alert(`${JSON.stringify(values.filial_id)}`);
+  };
+
   return (
-    <>
-      <Autocomplete
-        options={data}
-        value={valueSelect || null}
-        onChange={(event, newValue) => {
-          setValueSelect(newValue);
-          props.setCurrentFilial(newValue);
-        }}
-        inputValue={inputValue}
-        onInputChange={(event, newInputValue) => {
-          setInputValue(newInputValue);
-        }}
-        getOptionSelected={(option, value) =>
-          option.id === value.id || option.id === valueSelect.id
-        }
-        getOptionLabel={(option) => `${option.id} - ${option.nome_fantasia}`}
-        style={{ width: 300 }}
-        renderInput={(params) => (
-          <TextField {...params} label="Filial" variant="outlined" size="small"/>
-        )}
-      />
-    </>
+    <Formik initialValues={valueSelect} onSubmit={submit}>
+      {({ handleChange, values, setFieldValue }) => (
+        <Form>
+          <Autocomplete
+            id='filial_id'
+            options={data}
+            onChange={(event, newValue) => {
+              setValueSelect(
+                "filial_id",
+                newValue !== null ? newValue : valueSelect.filial_id
+              );
+              props.setCurrentFilial(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            getOptionSelected={(option, value) =>
+              option.id === value.id || option.id === valueSelect.id
+            }
+            getOptionLabel={(option) => `${option.id} - ${option.nome_fantasia}`}
+            style={{ width: 300 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Filial" variant="outlined" size="small" />
+            )}
+          />
+        </Form>
+      )}
+    </Formik>
   );
 };
 
